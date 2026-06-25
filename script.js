f6bc9195bff63bbfa155d5d4e01a7244e66c85e4
byancaconceicao-png <byanca.conceicao@escola.pr.gov.br>
2026-06-25 11:45:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script simples para interação, se necessário no futuro
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('A página de notícias foi carregada com sucesso.');
+});
